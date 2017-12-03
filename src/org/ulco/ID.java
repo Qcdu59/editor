54a227d489cfef8859b80b2d96ea0b1874396ee4
Qcdu59 <quentin.alcb.59@hotmail.fr>
2017-12-03 17:54:27 +0100

    Quatrième tache fonctionnelle

--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -1,5 +1,14 @@
 package org.ulco;
 
 public class ID {
-    static public int ID = 0;
+    static private int ID = 0;
+    static private ID id = new ID();
+    private ID(){
+    }
+    static public ID returnInstance(){
+    	return id;
+    }
+    static public int generateID(){
+    	return returnInstance().ID++;
+    }
 }
